0402c7e852a55bb950a8cb4227f17c7e26ddacbb
vanquang-dev <vanquang07032000@gmail.com>
2020-12-22 17:00:43 +0700

    update

--- login.php
+++ login.php
@@ -15,12 +15,10 @@
 <head>
 	<?php head('Đăng nhập | Hust News'); ?>
 </head>
-<body style="overflow: hidden; background: url(views/assets/image/backgroud_login_admin.jpg) center center / cover;">
+<body style="background: url(views/assets/image/backgroud_login_admin.jpg) center center / cover;">
 	<div class="black">
 		<div class="navbar-account">
-			
-				<h1><a href="/bigex/" style="font-size: 40px; color: #ffffffad; font-family: ui-sans-serif;">Hust News</a></h1>
-			
+			<h1><a href="/bigex/" style="font-size: 40px; color: #ffffffad; font-family: ui-sans-serif;">Hust News</a></h1>
 		</div>
 		<div class="container-account">
 			<h1 id="status">ĐĂNG NHẬP</h1>
@@ -33,7 +31,7 @@
 				<div class="input-login">
 					<i class="gg-lock faa-pulse animated" style="top: 36px;left: 48px;"></i><input id="password"  class="input_login" type="password" placeholder="Password" style="width: 79%;">
 				</div>
-				<button id="login" type="button" style="margin-top: 10px; background: linear-gradient(45deg, rgb(19 170 110) 0%, rgb(27 124 145) 100%);">Đăng nhập</button>
+				<button id="login" type="button" style="margin-top: 10px; background: linear-gradient(45deg, rgb(19 105 170) 0%, rgb(24 101 118) 100%);">Đăng nhập</button>
 				<div class="footer-login" style="position: absolute; top: 63%; left: 32px;">
 	            	<a href="register" style="color: #673ab7; margin-right: 170px;">Bạn chưa có tài khoản?</a>
 	            	<a href="#" style="color: #673ab7;">Quên mật khẩu?</a>
